a3d2154d254f19bc90fb920b99017dbe9b0d1412
jguih <gui.kcoal+github@hotmail.com>
2024-08-07 00:07:13 -0300

    Update sidebar

--- src/shared/components/sidebar/app/app-sidebar.tsx
+++ src/shared/components/sidebar/app/app-sidebar.tsx
@@ -4,11 +4,13 @@ import { FC, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { ThemeToggle } from "@/src/theme/components/theme-toggle";
 import { Typography } from "../../typography/typography";
-import { buildClassNames } from "../../utils/build-class-names";
 import { Button } from "../../button/button";
+import { buildClassNames } from "../../utils/build-class-names";
 
 export const AppSidebar: FC = () => {
   const [open, setOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const unmount = () => setMounted(false);
 
   useEffect(() => {
     if (!document) return;
@@ -16,31 +18,37 @@ export const AppSidebar: FC = () => {
     const toggler = document.getElementById("appsidebar-toggler");
     if (toggler instanceof HTMLButtonElement) {
       toggler.onclick = (ev) => {
-        setOpen(!open);
+        setMounted(!mounted);
+        if (!open) setOpen(true);
       };
     }
   }, []);
 
   const sidebarClasses = buildClassNames(
-    { [styles.opened]: open === true },
+    { [styles.in]: mounted === true, [styles.out]: mounted === false },
     styles.sidebar
   );
 
   const backdropClasses = buildClassNames(
-    {
-      [styles.opened]: open === true,
-    },
+    { [styles.in]: mounted === true, [styles.out]: mounted === false },
     styles.backdrop
   );
 
+  if (!open) return;
+
   return (
-    <div className={sidebarClasses}>
+    <div
+      className={sidebarClasses}
+      onAnimationEnd={() => {
+        if (!mounted) setOpen(false);
+      }}
+    >
       <div className={styles["sidebar-header"]}>
         <Typography>Sidebar</Typography>
-        <Button onClick={() => setOpen(false)}>Close</Button>
+        <Button onClick={unmount}>Close</Button>
       </div>
       <ThemeToggle />
-      <div className={backdropClasses} onClick={() => setOpen(false)} />
+      <div className={backdropClasses} onClick={unmount} />
     </div>
   );
 };
