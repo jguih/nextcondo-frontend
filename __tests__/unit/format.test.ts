c3ead5816df08bb5d8b77ac89312b377148d4913
José Guilherme <gui.kcoal+github@hotmail.com>
2024-08-18 20:07:36 -0300

    Fix typo

--- __tests__/unit/format.test.ts
+++ __tests__/unit/format.test.ts
@@ -3,14 +3,14 @@ import { format } from "@/src/localization/utils";
 describe("format", () => {
   it("replaces variables on a string", () => {
     const text =
-      "I have {{ orangesCount }} oranges. I will give then to {{ peopleCount }} people tomorrow at {{ time }}.";
+      "I have {{ orangesCount }} oranges. I will give them to {{ peopleCount }} people tomorrow at {{ time }}.";
     const formatted = format(text, {
       orangesCount: 28,
       peopleCount: 12,
       time: "12pm",
     });
     expect(formatted).toBe(
-      "I have 28 oranges. I will give then to 12 people tomorrow at 12pm."
+      "I have 28 oranges. I will give them to 12 people tomorrow at 12pm."
     );
   });
 });
